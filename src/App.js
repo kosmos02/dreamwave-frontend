7e3a4869b9f54a7520e43330a8fb50f4e8286437
kosmos02 <alexandrgabe@gmail.com>
2021-03-25 17:38:37 -0600

    made menu on account button and set content in Grid

--- src/App.js
+++ src/App.js
@@ -3,30 +3,38 @@ import './App.css';
 // import "bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 import NavBar from './components/navbar'
-import { Typography, Button, CssBaseline,} from '@material-ui/core';
+import { Typography, Button, CssBaseline, Grid, } from '@material-ui/core';
 
 
 class App extends Component {
-  
+
   render() {
     return (
       <Router>
-        <CssBaseline/>
-        <NavBar/>
-        <div className="title">
-          <Typography variant="h1">DreamWave</Typography>
-        </div>
-        
-        <div id="journal-about">
-          <Typography id= "journal-text" variant='body1'>
-            Here you can make a post about the dream you had. 
-            It includes the date, whether it was a positive dream, neutral, or nightmare,
-            if it was a lucid dream (where you have full or partial control), a description, 
-            and any associated tags you want to keep track of. 
-            Friends can comment on your dream to help you interpret it! 
-            The information you put here will be used for analytics.
+        <CssBaseline />
+        <NavBar />
+        <Grid
+          container
+          direction='column'
+          justify='center'
+          alignItems='center'
+        >
+          <div className="title">
+            <Typography variant="h1">DreamWave</Typography>
+          </div>
+
+          <div id="journal-about">
+            <Typography id="journal-text" variant='body1'>
+              Here you can make a post about the dream you had.
+              It includes the date, whether it was a positive dream, neutral, or nightmare,
+              if it was a lucid dream (where you have full or partial control), a description,
+              and any associated tags you want to keep track of.
+              Friends can comment on your dream to help you interpret it!
+              The information you put here will be used for analytics.
           </Typography>
-        </div>
+          </div>
+        </Grid>
+
       </Router>
 
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import { useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
-import { Button, makeStyles } from '@material-ui/core'
+import { Button, makeStyles, Menu, MenuItem } from '@material-ui/core'
 
 const useStyles = makeStyles({
     buttonText: {
@@ -11,20 +12,38 @@ const useStyles = makeStyles({
 })
 
 const NavBar = () => {
-    const classes = useStyles()
+    const [anchorEl, setAnchorEl] = useState(null);
 
-    return(
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
         <div>
             <AppBar position="static">
                 <Toolbar>
                     <Typography variant="h5" color="inherit">
                         DreamWave
                     </Typography>
-                        <Button>Journal</Button>
-                        <Button>Dictionary</Button> 
-                        <Button>Analytics</Button>
-                        <Button>About</Button>
-                        <Button>Logout</Button>
+                    <Button>Journal</Button>
+                    <Button>Dictionary</Button>
+                    <Button>Analytics</Button>
+                    <Button>About</Button>
+                    <Button aria-controls="simple-menu" aria-haspopup='true' onClick={handleClick}>Account</Button>
+                    <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        <MenuItem>Profile</MenuItem>
+                        <MenuItem>Logout</MenuItem>
+                    </Menu>
                 </Toolbar>
             </AppBar>
         </div>
